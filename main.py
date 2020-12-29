127abe042c931cbe95405c4e0ee749f4a0037970
anirudhasj441 <anirudhasj441@gmail.com>
2020-12-29 11:05:57 +0530

    add some code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# this is maim file
+from tkinter import Tk,filedialog,Frame
+from pytube import YouTube
+
+root = Tk()
+frame = Frame()
+entry = filedialog.askdirectory()
+frame.pack()
+print(entry)
+
+
+
+root.mainloop()
