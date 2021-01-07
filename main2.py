d8e851b47c601dea529db6f15e3462d442da72e4
anirudhasj441 <anirudhasj441@gmail.com>
2021-01-07 20:09:21 +0530

    Update

--- main2.py
+++ main2.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter import filedialog
 import tkinter.ttk
 from pytube import YouTube
+import os
 
 # functions
 def setStatus(statusvar):
@@ -9,11 +11,9 @@ def setStatus(statusvar):
 
 def getVideo(url):
     try:
-        status_progress.pack(side="left",padx=20)
-        status_progress.update()
-        status_progress.start(10)
         setStatus("Connecting...")
         yt = YouTube(url)
+        print(yt.length)
         # status_progress.stop()
         print(yt.title)
     except Exception as e:
@@ -21,34 +21,74 @@ def getVideo(url):
         print(e)
     else:
         setStatus("Connected")
-        status_progress.stop()
-        # status_progress.pack_forget()
-# static variables
-root = Tk()
-root.geometry("300x300")
-root.title("YouTube Downloder")
-status = StringVar()
+        setDetails(yt)
 
-# Frames
-main_frame = Frame(root)
-main_frame.pack(fill="both",pady=25,padx=50)
-status_frame = Frame(root,bg="gray")
-status_frame.pack(side="bottom",fill="x")
+def downloadVideo(yt):
+    video = yt.streams.filter(progressive = True, file_extension = "mp4").first()
+    yt.register_on_progress_callback(downloadProgress)
+    yt.register_on_complete_callback(downloadComplete)
+    video.download(download_path)
 
-# widgets main frame
-Label(main_frame,text="URL : ").grid(row=0,column=0)
-url = Entry(main_frame)
-url.grid(row=0,column=1,ipady=3)
-Button(main_frame,text="Get Video",command=lambda:getVideo(url.get())).grid(row=1,column=0,columnspan=2,pady=2)
+def downloadProgress(stream, chunk, bytes_remaining):
+    setStatus("Downloading")
+    size = stream.filesize
+    progress = tkinter.ttk.Progressbar(download_frame,length=400,maximum=size,value=size-bytes_remaining)
+    progress.update()
 
-# widgets status frame
-statusLabel = Label(status_frame,textvariable=status,bg="gray")
-statusLabel.pack(side="left")
-status_progress = tkinter.ttk.Progressbar(status_frame,length=100,mode="indeterminate")
-setStatus("Ready")
+def downloadComplete(stream,file_path):
+    setStatus("Downloaded in "+file_path)
+
+def setDetails(yt):
+    download_folder.set(download_path)
+    video_title.set(yt.title)
+    video = yt.streams.filter(progressive = True, file_extension = "mp4").first()
+    size = format(int(video.filesize)/1000000,".2f")
+    video_size.set(str(size)+"mb")
+    Label(details_frame,text="Title : ").grid(row=0,column=0,padx=10,pady=5)
+    Label(details_frame,textvariable=video_title,wraplength=300).grid(row=0,column=1,columnspan=2,padx=10,pady=5)
+    Label(details_frame,text="Size : ").grid(row=1,column=0,padx=10,pady=10)
+    Label(details_frame,textvariable=video_size).grid(row=1,column=1,padx=10,pady=5)
+    Label(details_frame,text="Download Folder : ",wraplength=200).grid(row=2,column=0,padx=10,pady=5)
+    Entry(details_frame,textvariable=download_folder,width=30).grid(row=2,column=1,padx=10,pady=5,ipady=3)
+    Button(details_frame,text="Browse",command=browse_folder).grid(row=2,column=2)
+    Button(details_frame,text="Download",bg="green",fg="white",command=lambda:downloadVideo(yt)).grid(row=3,column=0,columnspan=3)
+
+def browse_folder():
+    global download_path
+    download_path = filedialog.askdirectory()
+    download_folder.set(download_path)
 
 
+if __name__ == "__main__":
+    # static variables
+    root = Tk()
+    root.geometry("600x550")
+    root.title("YouTube Downloder")
+    status = StringVar()
+    video_title = StringVar()
+    video_size = StringVar()
+    download_folder = StringVar()
+    download_path = "/home/devil/Downloads"
 
+    # Frames
+    main_frame = Frame(root)
+    main_frame.pack(fill="both",pady=25,padx=50)
+    status_frame = Frame(root,bg="gray")
+    status_frame.pack(side="bottom",fill="x")
+    details_frame = Frame(root)
+    details_frame.pack(fill=BOTH,pady=15,padx=45)
+    download_frame = Frame(root)
+    download_frame.pack(fill=BOTH)
 
+    # widgets main frame
+    Label(main_frame,text="URL : ").grid(row=0,column=0)
+    url = Entry(main_frame,width=50)
+    url.grid(row=0,column=1,ipady=3)
+    Button(main_frame,text="Get Video",command=lambda:getVideo(url.get())).grid(row=1,column=1,columnspan=2,pady=2)
 
-root.mainloop()
+    # widgets status frame
+    statusLabel = Label(status_frame,textvariable=status,bg="gray")
+    statusLabel.pack(side="left")
+    setStatus("Ready")
+
+    root.mainloop()
