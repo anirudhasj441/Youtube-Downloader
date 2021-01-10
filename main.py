065fc2e1e9bd19939e59ce99690ebc61aee248b1
anirudhasj441 <anirudhasj441@gmail.com>
2021-01-10 13:46:40 +0530

    update

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ def getVideo(url):
     try:
         setStatus("Connecting...")
         yt = YouTube(url)
-        print(yt.length)
-        # status_progress.stop()
-        print(yt.title)
     except Exception as e:
         setStatus("Connection Failed")
         print(e)
@@ -23,20 +20,27 @@ def getVideo(url):
         setStatus("Connected")
         setDetails(yt)
 
+def progressbar(value):
+    progressvar.set(value)
+    progress.update()
+    progress.update_idletasks()
+
 def downloadVideo(yt):
+    progress.grid(row=0,column=0)
     video = yt.streams.filter(progressive = True, file_extension = "mp4").first()
     yt.register_on_progress_callback(downloadProgress)
     yt.register_on_complete_callback(downloadComplete)
     video.download(download_path)
 
 def downloadProgress(stream, chunk, bytes_remaining):
-    setStatus("Downloading")
     size = stream.filesize
-    progress = tkinter.ttk.Progressbar(download_frame,length=400,maximum=size,value=size-bytes_remaining)
-    progress.update()
+    per = round((1-bytes_remaining/size)*100, 2)
+    setStatus("Downloading"+" "+str(round((size-bytes_remaining)/1000000,2))+"/"+str(round(size/1000000,2))+"MB")
+    per_var.set(str(per)+"%")
+    progressbar(per)
 
 def downloadComplete(stream,file_path):
-    setStatus("Downloaded in "+file_path)
+    setStatus("Download Complete")
 
 def setDetails(yt):
     download_folder.set(download_path)
@@ -51,13 +55,13 @@ def setDetails(yt):
     Label(details_frame,text="Download Folder : ",wraplength=200).grid(row=2,column=0,padx=10,pady=5)
     Entry(details_frame,textvariable=download_folder,width=30).grid(row=2,column=1,padx=10,pady=5,ipady=3)
     Button(details_frame,text="Browse",command=browse_folder).grid(row=2,column=2)
-    Button(details_frame,text="Download",bg="green",fg="white",command=lambda:downloadVideo(yt)).grid(row=3,column=0,columnspan=3)
+    download_button = Button(details_frame,text="Download",bg="green",fg="white",command=lambda:downloadVideo(yt))
+    download_button.grid(row=3,column=0,columnspan=3)
 
 def browse_folder():
     global download_path
     download_path = filedialog.askdirectory()
     download_folder.set(download_path)
-
 
 if __name__ == "__main__":
     # static variables
@@ -68,6 +72,8 @@ if __name__ == "__main__":
     video_title = StringVar()
     video_size = StringVar()
     download_folder = StringVar()
+    per_var = StringVar()
+    progressvar = DoubleVar()
     download_path = "/home/devil/Downloads"
 
     # Frames
@@ -78,7 +84,7 @@ if __name__ == "__main__":
     details_frame = Frame(root)
     details_frame.pack(fill=BOTH,pady=15,padx=45)
     download_frame = Frame(root)
-    download_frame.pack(fill=BOTH)
+    download_frame.pack(fill=BOTH,pady=15,padx=45)
 
     # widgets main frame
     Label(main_frame,text="URL : ").grid(row=0,column=0)
@@ -90,5 +96,10 @@ if __name__ == "__main__":
     statusLabel = Label(status_frame,textvariable=status,bg="gray")
     statusLabel.pack(side="left")
     setStatus("Ready")
+
+    # widgets download frame
+    progress = tkinter.ttk.Progressbar(download_frame,variable=progressvar,length=400,maximum=100,value=0)
+    per_label = Label(download_frame,textvariable=per_var)
+    per_label.grid(row=0,column=1)
 
     root.mainloop()
